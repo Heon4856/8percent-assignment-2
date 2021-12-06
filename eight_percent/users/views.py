9168da3c11c270e070d1a6850f8dd3d006b8987b
Sungeheon <elon4856@gmail.com>
2021-12-06 20:01:11 +0900

    refactor: 회원가입 view drf를 통한 간결화.

--- eight_percent/users/views.py
+++ eight_percent/users/views.py
@@ -3,12 +3,20 @@ import bcrypt
 import jwt
 import re
 from json import JSONDecodeError
+from rest_framework.generics import CreateAPIView
 
 from django.views import View
 from django.http  import JsonResponse
 
 from .models import Users
 from my_settings import SECRET_KEY, HASHING_ALGORITHM
+from .serializers import UserSerializer
+
+
+class SignUpView(CreateAPIView):
+    model = Users
+    serializer_class = UserSerializer
+
 
 
 class LoginView(View):
@@ -27,37 +35,3 @@ class LoginView(View):
         except Users.DoesNotExist:
             return JsonResponse({'message': 'USER_DOES_NOT_EXIST'}, status=401)
 
-
-class SignUpView(View):
-    def post(self, request):
-        try:
-            data = json.loads(request.body)
-
-            name = data['name']
-            password = data['password']
-
-            if not name or not password:
-                return JsonResponse({'message': 'EMPTY_VALUE'}, status=400)
-
-            p_name = re.compile(r'^[가-힣a-zA-Z]{2,20}$')
-            p_password = re.compile(r'^(?=.*[!-/:-@])(?!.*[ㄱ-ㅣ가-힣]).{8,20}$')
-
-            if not p_name.match(name):
-                return JsonResponse({'message': 'INVALID_NAME_FORMAT'}, status=400)
-            if not p_password.match(password):
-                return JsonResponse({'message': 'INVALID_PASSWORD_FORMAT'}, status=400)
-
-
-            hashed_pw = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-            decoded_hashed_pw = hashed_pw.decode('utf-8')
-
-            Users.objects.create(
-                name=name,
-                password=decoded_hashed_pw,
-            )
-            return JsonResponse({'message': 'SUCCESS'}, status=201)
-
-        except KeyError:
-            return JsonResponse({'message': 'KEY_ERROR'}, status=400)
-        except JSONDecodeError:
-            return JsonResponse({'message': 'JSON_DECODE_ERROR'}, status=400)
