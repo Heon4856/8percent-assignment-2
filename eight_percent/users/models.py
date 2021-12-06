a4cc396c9e768b9f6fa8c3cdb3b0a70c0b4a4ec9
Sungeheon <elon4856@gmail.com>
2021-12-06 19:59:26 +0900

    refactor: User의 name필드에 unique=True.

--- eight_percent/users/models.py
+++ eight_percent/users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Users(models.Model):
-    name = models.CharField(max_length=10)
+    name = models.CharField(max_length=10, unique=True)
     password = models.CharField(max_length=100)
 
     class Meta:
