26ebc22bd6052b6b76c0bf631b5db92d9b98c690
Sungeheon <elon4856@gmail.com>
2021-12-07 11:26:45 +0900

    add: validation 추가 및 request일때와, response일때 finetuning

--- eight_percent/transaction/serializers.py
+++ eight_percent/transaction/serializers.py
@@ -1,9 +1,21 @@
 from rest_framework.serializers import ModelSerializer
 
+from .exceptions import BadRequestException
 from .models import Account
 
 
 class AccountSerializer(ModelSerializer):
     class Meta:
         model = Account
-        fields = '__all__'
+        fields = ['password', 'number', 'balance', 'user']
+        extra_kwargs = {'password': {'write_only': True}}
+        read_only_fields = ['number', 'balance', 'user']
+
+
+    def validate(self, data):
+        password   = data.get('password')
+
+        if password is None:
+            raise BadRequestException('name or number')
+
+        return data
