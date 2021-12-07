56a6523826a96815995581e4b7c2b3ace2037605
Sungeheon <elon4856@gmail.com>
2021-12-07 20:48:56 +0900

    refactor: 쓸데없는 문구 삭제

--- eight_percent/transaction/exceptions.py
+++ eight_percent/transaction/exceptions.py
@@ -5,5 +5,5 @@ class BadRequestException(APIException):
     status_code = 400
 
     def __init__(self, field_name):
-        self.default_detail = f"{field_name} is invalid"
+        self.default_detail = f"{field_name}"
         super(BadRequestException, self).__init__()
