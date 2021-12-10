73fd400c0dd55c83a197c2ef60224e823bc4eb69
Sungeheon <elon4856@gmail.com>
2021-12-10 16:06:34 +0900

    refactoring: field 타입 변경, 인덱스변경 - decimal -- >positivbigint가 더적절함 - 파티셔닝된 테이블, 외래키 제거. mysql 특성상, 파티셔닝시 외래키 못씀 - 인덱스 변경. created_at은 시간이 찍히므로 cardinality가 너무 낮다. 그리고 cardinality가 높은 순서대로 하는 것이 좋으므로, 그렇게 변경.

--- eight_percent/transaction/models.py
+++ eight_percent/transaction/models.py
@@ -9,8 +9,8 @@ from users.models import Users
 class Account(models.Model):
     number = models.CharField(max_length=30)
     password = models.CharField(max_length=100)
-    balance = models.DecimalField(max_digits=20, decimal_places=2, default=0)
-    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    balance = models.PositiveBigIntegerField()
+    user = models.ForeignKey(Users)
 
     class Meta:
         db_table = 'accounts'
@@ -29,18 +29,18 @@ class TransactionType(models.Model):
 
 @architect.install('partition', type='range', subtype='date', constraint='month', column='created_at')
 class Transaction(models.Model):
-    amount = models.DecimalField(max_digits=20, decimal_places=2)
+    amount = models.PositiveBigIntegerField()
     created_at = models.DateTimeField(default=timezone.now)
-    description = models.CharField(max_length=50)
-    counterparty = models.CharField(max_length=20)
-    balance = models.DecimalField(max_digits=20, decimal_places=2, default=0)
-    account = models.ForeignKey('Account', on_delete=models.CASCADE)
-    transaction_type = models.ForeignKey('TransactionType', on_delete=models.CASCADE)
+    description = models.CharField(max_length=7)
+    counterparty = models.CharField(max_length=10)
+    balance = models.PositiveBigIntegerField()
+    account = models.CharField(max_length=30)
+    transaction_type = models.IntegerField()
 
     class Meta:
         db_table = 'transactions'
         indexes = [
-            models.Index(fields=['created_at', ]),
+            models.Index(fields=['transaction_type', ]),
             models.Index(fields=['account', ]),
-            models.Index(fields=['transaction_type', ])
+
         ]
