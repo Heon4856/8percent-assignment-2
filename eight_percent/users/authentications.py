b208d5d65aa3878275c3de5f1b24eace482d8d3f
Sungeheon <elon4856@gmail.com>
2021-12-10 21:02:22 +0900

    rafactor: exception에 대해 추가. - 토큰이 없을 시. - 토큰이 잘못됏을시.

--- eight_percent/users/authentications.py
+++ eight_percent/users/authentications.py
@@ -1,5 +1,5 @@
 import jwt
-from rest_framework import authentication
+from rest_framework import authentication, exceptions
 
 from .models import Users
 from my_settings import SECRET_KEY, HASHING_ALGORITHM
@@ -8,9 +8,12 @@ class BankingAuthentication(authentication.TokenAuthentication):
     def authenticate(self, request):
         access_token = request.headers.get('Authorization')
         if access_token:
-            access_token = access_token[len("Bearer "):]
-            payload = jwt.decode(access_token, SECRET_KEY, algorithms=HASHING_ALGORITHM)
-            userid = payload['id']
-            user = Users.objects.get(id=userid)
-            return user, None
-        return None
+            try:
+                access_token = access_token[len("Bearer "):]
+                payload = jwt.decode(access_token, SECRET_KEY, algorithms=HASHING_ALGORITHM)
+                userid = payload['id']
+                user = Users.objects.get(id=userid)
+                return user, None
+            except Exception:
+                raise exceptions.AuthenticationFailed("No such user")
+        raise exceptions.AuthenticationFailed("No token")
