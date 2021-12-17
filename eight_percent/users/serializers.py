ce4b231306d86ad4e247956aa2171aa6c6fd5b12
Sungeheon <elon4856@gmail.com>
2021-12-18 01:33:30 +0900

    refactor: validate 추가

--- eight_percent/users/serializers.py
+++ eight_percent/users/serializers.py
@@ -1,11 +1,21 @@
 import bcrypt
 from rest_framework import serializers
 
+from .exceptions import BadRequestException
 from .models import Users
 
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField()
+
+    def validate(self, data):
+        if not data.get('name') or not data.get('password'):
+            raise BadRequestException({'message': '이름이나 비밀번호가 없습니다.'})
+        if  len(data.get('name'))<2:
+            raise BadRequestException({'message': '이름을 제대로 적어주세요/'})
+
+        return data
+
 
     def create(self, validated_data):
         new_salt = bcrypt.gensalt(12)
