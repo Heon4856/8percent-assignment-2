2fa8407df42d4f89a8c1d017a51c1b44366282eb
Sungeheon <elon4856@gmail.com>
2021-12-18 18:31:57 +0900

    add: not found exception, wrong password exception 추가.

--- eight_percent/users/exceptions.py
+++ eight_percent/users/exceptions.py
@@ -7,3 +7,19 @@ class BadRequestException(APIException):
     def __init__(self, field_name):
         self.default_detail = f"{field_name}"
         super(BadRequestException, self).__init__()
+
+
+class NotFoundException(APIException):
+    status_code = 404
+
+    def __init__(self, field_name):
+        self.default_detail = f"{field_name}"
+        super(NotFoundException, self).__init__()
+
+
+class WrongPasswordException(APIException):
+    status_code = 400
+
+    def __init__(self, field_name):
+        self.default_detail = f"{field_name}"
+        super(WrongPasswordException, self).__init__()
