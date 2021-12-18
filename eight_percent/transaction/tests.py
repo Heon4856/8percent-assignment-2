b2ed8be3c955c87bb531ab4a53096c5db3e02a03
Sungeheon <elon4856@gmail.com>
2021-12-18 20:32:06 +0900

    test: account read unittest 작성.

--- eight_percent/transaction/tests.py
+++ eight_percent/transaction/tests.py
@@ -3,6 +3,7 @@ import jwt
 from rest_framework.test import APIClient
 
 from my_settings import SECRET_KEY, HASHING_ALGORITHM
+from transaction.models import Account
 from users.models import Users
 from util.test import BaseTestCase
 
@@ -16,6 +17,12 @@ class AccountTest(BaseTestCase):
         self.unvalid_user = Users.objects.create(
             name='unvalid',
             password=bcrypt.hashpw('1234abcd!!!!'.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+        )
+
+        self.account = Account.objects.create(
+            user=self.valid_user,
+            password=bcrypt.hashpw("1234".encode('utf-8'), bcrypt.gensalt()).decode('utf-8'),
+            number="1234-12-123456"
         )
 
         self.valid_token = "Bearer " + jwt.encode({'id': self.valid_user.id}, SECRET_KEY, algorithm=HASHING_ALGORITHM)
@@ -47,3 +54,15 @@ class AccountTest(BaseTestCase):
         response = self.client.post("/transaction/account", account_password, format='json')
         self.assertEqual(response.status_code, 401)
         self.assertEqual(response.json(), {"detail": "No token"})
+
+    def test_read_account_list_success(self):
+        response = self.client.get("/transaction/account")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), [{"number": "1234-12-123456", "balance": 0, "user_name": "elon4856"}])
+
+        self.client.credentials(HTTP_AUTHORIZATION=self.unvalid_token)
+        response = self.client.get("/transaction/account")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), [])
+
+
